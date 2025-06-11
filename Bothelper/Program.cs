1599f86ce591f027aee0327670d5884a5c27a818
05anton05 <anton0505Postavets@gmail.com>
2025-06-11 23:03:16 +0300

    New version of bot

--- Bothelper/Program.cs
+++ Bothelper/Program.cs
@@ -21,6 +21,11 @@ namespace Bothelper
             Console.WriteLine(" 2 - Таблиця множення числа");
             Console.WriteLine(" 3 - Порахувати суму чисел");
             Console.WriteLine(" 4 - Вивести моє ім’я задом наперед");
+            Console.WriteLine(" 5 - Знайти число в массивi");
+            Console.WriteLine(" 6 - Показати температуру на недiлю вперед");
+            Console.WriteLine(" 7 - Розклад занять для нового життя");
+            Console.WriteLine(" 8 - Отримати вмiст кошику смачного мiлкшейку");
+            Console.WriteLine(" 9 - Отримати телефонну книгу топових людей");
             Console.WriteLine(" 0 - Вийти з програми");
            
           
@@ -39,7 +44,7 @@ namespace Bothelper
                         {
                             Console.WriteLine(firstNumber + " - Число непарне!");
                         }
-                       
+
                         break;
                     case 2:
                         Console.WriteLine("Пропишiть число:");
@@ -49,7 +54,7 @@ namespace Bothelper
                             int result = secondNumber * a;
                             Console.WriteLine($"{secondNumber} x {a} = {result} ");
                         }
-                        
+
                         break;
                     case 3:
                         Console.WriteLine("Введiть будь-яке число:");
@@ -60,7 +65,7 @@ namespace Bothelper
                             startNumber = startNumber + b;
                         }
                         Console.WriteLine("cумма чисел вiд 1 до " + thirdNumber + " = " + startNumber);
-                        
+
                         break;
                     case 4:
                         Console.Write("Ввeдiть iм'я: ");
@@ -70,7 +75,92 @@ namespace Bothelper
                         {
                             Console.Write(four);
                         }
-                            Console.WriteLine();
+                        Console.WriteLine();
+                        break;
+                    case 5:
+                        int[] array5 = { -20, -10, 0, 10, 20, 30, 40, 50, 60 };
+                        Console.Write(" массив чисел: ");
+                        for (int i = 0; i < array5.Length; i++)
+                        {
+                            Console.Write(" " + array5[i] + ";");
+                        }
+
+                        Console.WriteLine("\n пропишить число, яке хочете знайти в массивi: ");
+                        int number = int.Parse(Console.ReadLine());
+                        int leftSide = 0;
+                        int rightSide = array5.Length - 1;
+                        while (leftSide <= rightSide)
+                        {
+                            int center = (leftSide + rightSide) / 2;
+                            if (array5[center] == number)
+                            {
+                                Console.WriteLine("Iндекс числа " + number + " в массивi: " + center);
+                                break;
+                            }
+                            else if (array5[center] < number)
+                            {
+                                leftSide = center + 1;
+                            }
+                            else
+                            {
+                                rightSide = center - 1;
+                            }
+                        }
+                        if (leftSide > rightSide)
+                        {
+                            Console.WriteLine("Число " + number + " не знайдено в массивi.");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Число " + number + " знайдено в массивi.");
+                        }
+                        break;
+                    case 6:
+                        double[] temps = { 29.5, 31, 30.5, 33.5, 30 };
+                        string[] DayOfWeek = { "Понеділок:", "Вівторок:", "Середа:", "Четвер:", "П'ятниця:" };
+                        Console.WriteLine("Температура на тиждень вперед:");
+                        for (int i = 0; i < temps.Length; i++)
+                        {
+                            Console.WriteLine($"{DayOfWeek[i]}" + $" {temps[i]}" + "°C");
+                        }
+                        break;
+                    case 7:
+                        string[,] schedule = {
+                            {"Понедiлок:", "Кетасушi"},
+                            {"Вiвторок:", "Англiйська та айтiшка"},
+                            {"Середа:", "айтiшка та заняття з ментором" },
+                            {"Четвер:", "Англiйcька та кетасушi" },
+                            {"П'ятниця:", "Кетасушi" },
+                            {"Суббота:", "кетасушi" },
+                            {"Недiля:", "Вiдпочинок та айтiшка" } };
+                        Console.WriteLine("Розклад занять на тиждень:");
+                        foreach(string succes in schedule)
+                        {
+                            Console.WriteLine(succes);
+                        }
+                        break;
+                    case 8:
+                        List<string> cart = new List<string> { "Банан", "Кiвi", "Ананасовий сiк", "Шпинат" };
+                        Console.WriteLine("Вмiст кошика:");
+                        foreach (string item in cart)
+                        {
+                            Console.WriteLine("-" + item);
+                        }
+                        break;
+                    case 9:
+                        Dictionary<string, string> phoneBook = new Dictionary<string, string>()
+                        {
+                        {"Антон:","380509521905"},
+                        {"Мiша:","380684207236"},
+                        {"Марина:","380978193567"},
+                        {"Даша:","380955482582"},
+                        {"Сергiй:","380635337296"}};
+                        Console.WriteLine("Телефонна книга:");
+                        foreach (var list in phoneBook)
+                        {
+                            Console.WriteLine(list);
+                        }
+
                         break;
                     case 0:
                         Console.WriteLine("Програму завершено, дякую за користування!");
@@ -92,44 +182,8 @@ namespace Bothelper
                     isContinueWork = false;
                     Console.Clear();
                 }
-                //ArrayS.
                 
-                int[] array5 = { -20, -10, 0, 10, 20, 30, 40, 50, 60 };
-                Console.Write(" массив чисел: ");
-                for (int i = 0; i < array5.Length; i++)
-                {
-                    Console.Write(" " + array5[i] + ";");
-                }
-
-                Console.WriteLine("\n введите число, которое хотите найти в массиве: ");
-                int number = int.Parse(Console.ReadLine());
-                int leftSide = 0;
-                int rightSide = array5.Length - 1;
-                while (leftSide <= rightSide)
-                {
-                    int center = (leftSide + rightSide) / 2;
-                    if (array5[center] == number)
-                    {
-                        Console.WriteLine("Индекс числа " + number + " в массиве: " + center);
-                        break;
-                    }
-                    else if (array5[center] < number)
-                    {
-                        leftSide = center + 1;
-                    }
-                    else
-                    {
-                        rightSide = center - 1;
-                    }
-                }
-                if (leftSide > rightSide)
-                {
-                    Console.WriteLine("Число " + number + " не найдено в массиве.");
-                }
-                else
-                {
-                    Console.WriteLine("Число " + number + " найдено в массиве.");
-                }
+                
             }
 
         }
--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# Bothelper
+The new version of BotHelper.
+This version includes different parts of menu who you can use
+The methods who was used:
+- call a Console;
+- construction switch;
+- various cycles(for,while,foreach);
+- binary search of array
+- method of working with collections;
+- if,else;
+The rules are follow coding style and code conventions.
